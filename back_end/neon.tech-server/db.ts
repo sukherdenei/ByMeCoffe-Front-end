5853a53b19a29a2b868348d5dec6046a441b3dd7
sukherdenei <sukherdene.ba@gmail.com>
2025-04-01 01:05:17 +0800

    util-db.ts-queryService.ts

--- back_end/neon.tech-server/db.ts
+++ back_end/neon.tech-server/db.ts
@@ -1,19 +1,19 @@
-import dotenv from "dotenv";
-import { Client } from "pg";
+// import dotenv from "dotenv";
+// import { Client } from "pg";
 
-dotenv.config();
+// dotenv.config();
 
-const connectionString = process.env.DATABASE_URL;
+// const connectionString = process.env.DATABASE_URL;
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL not set in .env file");
-}
+// if (!connectionString) {
+//   throw new Error("DATABASE_URL not set in .env file");
+// }
 
-export const getClient = (): Client => {
-  return new Client({
-    connectionString,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-};
+// export const getClient = (): Client => {
+//   return new Client({
+//     connectionString,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   });
+// };
