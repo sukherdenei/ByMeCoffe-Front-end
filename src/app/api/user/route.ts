671ed8c6ac46dcb12473ec7abec8ac5c0c6fb37c
24LP3201 24LP3201 <24LP3201@G304-CC0221.local>
2025-04-01 12:46:27 +0800

    Neon.tech-POST

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,14 +1,56 @@
+import { NextResponse } from "next/server";
 import { checkUser } from "../../../../back_end/controllers/user.controller";
 import { getUsers } from "../../../../back_end/user";
+import { runQuery } from "../../../../util/server/queryService";
+import { error } from "console";
 
-export async function GET() {
-  const users = await getUsers();
+// export async function GET() {
+//   const users = await getUsers();
 
-  return new Response(JSON.stringify({ data: users }));
-}
+//   return new Response(JSON.stringify({ data: users }));
+// }
 
 export async function POST(req: Request, res: Response) {
-  const body = await req.json();
-  console.log({ body });
-  return await checkUser({ email: body.email, password: body.password });
+  const {name, price} = await req.json();
+  try {
+    const query = `INSERT INTO "coffee" (name, price) VALUES ($1, $2)`
+    const addCoffee = await runQuery(query,[name,price] )
+
+    return new NextResponse(JSON.stringify({user: addCoffee }));
+  } catch (error) {
+    return new NextResponse (JSON.stringify({error:"Failed to run query"}),{
+      status:500
+    })
+  }
+
+  // return await checkUser({ email: body.email, password: body.password });
 }
+
+export async function GET(): Promise<NextResponse> {
+  try {
+//     const createTable = 
+//       `CREATE TABLE "public"."Coffee" ("id" integer PRIMARY KEY,
+//   "name" varchar NOT NULL,
+//   "price" integer
+// );` ;
+const user = `SELECT id, email,password  FROM "coffee" WHERE name='latte' ORDER BY price;`;
+
+
+
+const getUser = await runQuery(user);
+if(getUser.length <=0){
+  return new NextResponse(JSON.stringify({error:"coffe not found"}),{
+    status:404,
+  })
+}
+
+    return new NextResponse(JSON.stringify({getUser:getUser}));
+  } catch (error) {
+    console.log("Failed to run query:",error)
+    return new NextResponse (JSON.stringify({error:"Failed to run query"}),{
+      status:500
+    })
+  }
+}
+
+
